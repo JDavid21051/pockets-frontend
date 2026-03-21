4a3f3082eb01b2e7f14056215747ebbaf9792845
David Pelaez <david.pelaezcumbe@gmail.com>
2026-03-21 18:57:10 -0500

    feat: init project feat: add nvmrc file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatDrawerContainer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatCheckbox, MatSidenavModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('pockets-frontend');
+  protected pageSizeOptions: Signal<number[]> = signal([5, 10, 25]);
+
+  constructor() {}
+
+  private addPageSizes(): void {
+    this.pageSizeOptions().push(50);
+    this.pageSizeOptions().push(100);
+    this.pageSizeOptions().push(200);
+  }
 }
