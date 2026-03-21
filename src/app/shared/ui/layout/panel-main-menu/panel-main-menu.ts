59569df03a5eefa3ccf41d7ad31dc6125b23281e
David Pelaez <david.pelaezcumbe@gmail.com>
2026-03-21 18:58:06 -0500

    feat: setup multiple material themes in runtimes feat: create panel-main component

---
+++ src/app/shared/ui/layout/panel-main-menu/panel-main-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-main-menu',
+  imports: [],
+  templateUrl: './panel-main-menu.html',
+  styleUrl: './panel-main-menu.css',
+})
+export class PanelMainMenu {
+
+}
