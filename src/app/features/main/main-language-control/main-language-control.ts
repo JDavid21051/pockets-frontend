ef134fb18cabdec2e52e7d5340867f36b709c7eb
David Pelaez <david.pelaezcumbe@gmail.com>
2026-06-21 10:07:16 -0500

    feat: separates the responsibility for changing the language. feat: delete CSS file of main-language-control.ts

--- src/app/features/main/main-language-control/main-language-control.ts
+++ src/app/features/main/main-language-control/main-language-control.ts
@@ -1,23 +1,25 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { APP_MENU_LANG } from '@/infra/const/app-menu-lang.const';
 import { AllowedLanguagesEnum } from '@/domain/enums/allowed-languages.enum';
 import { cn } from '@/infra/parsers/css-class-name';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'krih-main-language-control',
-  imports: [TranslatePipe, MatIcon],
+  imports: [TranslatePipe, MatIcon, MatTooltip],
   templateUrl: './main-language-control.html',
-  styleUrl: './main-language-control.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLanguageControl {
+  private translate: TranslateService = inject(TranslateService);
   protected readonly cn = cn;
   readonly languagesList = APP_MENU_LANG;
   readonly langSelected = signal(AllowedLanguagesEnum.es);
 
   onClickChangeLanguage(selected: AllowedLanguagesEnum) {
+    this.translate.use(selected);
     this.langSelected.set(selected);
   }
 }
--- src/app/features/main/main-sidebar/main-sidebar.ts
+++ src/app/features/main/main-sidebar/main-sidebar.ts
@@ -1,19 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { cn } from '@/infra/parsers/css-class-name';
 import { PanelMainMenu } from '@/shared/ui/modules/panel-main-menu/panel-main-menu';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
-import { AllowedLanguagesEnum } from '@/domain/enums/allowed-languages.enum';
 import { MainSidebarController } from '@/infra/services/main-sidebar-control.service';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { ResponsiveControlService } from '@/infra/services/responsive-control.service';
 import { APP_MENU_MODULES } from '@/infra/const/app-menu-modules.const';
-import { APP_MENU_LANG } from '@/infra/const/app-menu-lang.const';
-import { MatTooltip } from '@angular/material/tooltip';
+import { MainLanguageControl } from '@/features/main/main-language-control/main-language-control';
 
 @Component({
   selector: 'krih-main-sidebar',
-  imports: [PanelMainMenu, MatMenuModule, MatIcon, TranslatePipe, MatTooltip],
+  imports: [PanelMainMenu, MatMenuModule, MatIcon, MainLanguageControl],
   templateUrl: './main-sidebar.html',
   styleUrl: './main-sidebar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,16 +18,8 @@ import { MatTooltip } from '@angular/material/tooltip';
 export class MainSidebar {
   private readonly responsiveService: ResponsiveControlService = inject(ResponsiveControlService);
   private readonly sidebarController: MainSidebarController = inject(MainSidebarController);
-  private readonly translate: TranslateService = inject(TranslateService);
   protected readonly menuItems = APP_MENU_MODULES;
   protected readonly isResponsive = this.responsiveService.isResponsive;
   protected readonly cn = cn;
   readonly sidebarOpen = this.sidebarController.state;
-  readonly langSelected = signal(AllowedLanguagesEnum.es);
-  readonly languagesList = APP_MENU_LANG;
-
-  onClickChangeLanguage(selected: AllowedLanguagesEnum) {
-    this.translate.use(selected);
-    this.langSelected.set(selected);
-  }
 }
