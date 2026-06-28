1e37e852100f2cca269dffe64a615cc0c27ed915
David Pelaez <david.pelaezcumbe@gmail.com>
2026-06-27 23:12:34 -0500

    feat: create the template for the list of accounts feat: create port interface

---
+++ src/app/core/application/ports/repository/accounts/accounts.port.ts
@@ -0,0 +1,15 @@
+/*
+ * Project:      pockets-frontend
+ * Developed by: Juan David Pelaez Cumbe
+ * Date:         6/27/2026
+ * Module name:  accounts.port.ts
+ * File name:    accounts.port
+ * IDE:          WebStorm
+ */
+
+import type { Observable } from 'rxjs';
+import type { AccountListModel } from '@/domain/models/accounts/accounts.model';
+
+export interface AccountsPort {
+  readonly list: () => Observable<AccountListModel[]>;
+}
---
+++ src/app/core/domain/models/accounts/accounts.model.ts
@@ -0,0 +1,12 @@
+/*
+ * Project:      pockets-frontend
+ * Developed by: Juan David Pelaez Cumbe
+ * Date:         6/27/2026
+ * Module name:  accounts.model.ts
+ * File name:    accounts.model
+ * IDE:          WebStorm
+ */
+
+export interface AccountListModel {
+  name: string;
+}
