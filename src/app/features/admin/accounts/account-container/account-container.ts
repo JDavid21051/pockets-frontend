1554a78002d2d7fc694ff1eb0ddcc44d9de7b402
David Pelaez <david.pelaezcumbe@gmail.com>
2026-06-29 22:20:21 -0500

    feat: add account list header feat: add account table config feat: add account table component

--- src/app/features/admin/accounts/account-container/account-container.ts
+++ src/app/features/admin/accounts/account-container/account-container.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccountsStore } from '@/application/store/accounts.store';
+import type { SkyColumnsConfig } from '@/domain/models/uix/sky-table.model';
+import type { SkyTableActionsType } from '@/domain/types/uix/table.type';
+import { StandardModuleHeader } from '@/shared/ui/modules/standard-header/standard-header';
+import { SpinnerDirective } from '@/infra/directives/spinner.directive';
+import { SkyTable } from '@/shared/ui/organisms/sky-table/sky-table';
+import {
+  ACCOUNT_CURRENCY_SYMBOL_MAP,
+  ACCOUNT_TYPE_MAP,
+} from '@/infra/const/account/accounts-map.const';
 
 @Component({
   selector: 'krih-account-container',
-  imports: [],
+  imports: [StandardModuleHeader, SpinnerDirective, SkyTable],
   templateUrl: './account-container.html',
   styleUrl: './account-container.css',
 })
-export class AccountContainer {}
+export class AccountContainer implements OnInit {
+  private readonly store = inject(AccountsStore);
+
+  protected columnsConfig: SkyColumnsConfig[] = [
+    { field: 'bank', header: 'shared.text.name', type: 'text', grow: 2 },
+    { field: 'status', header: 'shared.text.status.title', type: 'activePill', grow: 1 },
+    {
+      field: 'account_type',
+      header: 'shared.text.type',
+      type: 'mapNumber',
+      grow: 0.5,
+      mapValues: ACCOUNT_TYPE_MAP,
+    },
+    {
+      field: 'currency',
+      header: 'account.fields.currency.header',
+      type: 'mapNumber',
+      cssClass: 'text-center',
+      grow: 0.5,
+      mapValues: ACCOUNT_CURRENCY_SYMBOL_MAP,
+    },
+    { field: 'balance', header: 'account.fields.balance.label', type: 'text', grow: 1.5 },
+    { field: 'account_location', header: 'account.fields.location.label', type: 'text' },
+  ];
+
+  protected actionTable: SkyTableActionsType[] = ['update', 'delete'];
+  readonly dataTable = this.store.dataTableSource;
+  readonly getAccounts = this.store.getAccounts;
+  readonly loading = this.store.listLoading;
+
+  ngOnInit(): void {
+    this.getAccounts();
+  }
+}
