c6de0bb3da1dc942c204558702e64396958b7a11
David Pelaez <david.pelaezcumbe@gmail.com>
2026-06-29 22:19:46 -0500

    feat: create account store

---
+++ src/app/core/infra/repository/modules/accounts.repository.ts
@@ -0,0 +1,26 @@
+/*
+ * Project:      pockets-frontend
+ * Developed by: Juan David Pelaez Cumbe
+ * Date:         6/27/2026
+ * Module name:  accounts.repository.ts
+ * File name:    accounts.repository
+ * IDE:          WebStorm
+ */
+
+import { inject, Injectable } from '@angular/core';
+import { URL_SERVICES } from '@/infra/config/urls-services.config';
+import { CoreHttpClientService } from '@/infra/http/core-http-client.service';
+import type { AccountsPort } from '@/application/ports/repository/accounts/accounts.port';
+import type { AccountsListModel } from '@/domain/models/accounts/accounts.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountsRepository implements AccountsPort {
+  private readonly urls = URL_SERVICES.accounts;
+  private readonly http = inject(CoreHttpClientService);
+
+  list() {
+    return this.http.get<AccountsListModel[]>(this.urls.base + this.urls.list);
+  }
+}
